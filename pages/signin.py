a11e7e883a71f5ca46707be61e8a0447829f151f
Jillayne <jillayne2020@gmail.com>
2023-03-01 16:07:55 -0500

    added pseudo/code for signin and signout

--- pages/signin.py
+++ pages/signin.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
-# read csv of users
-
-# on button click submit, check if valid user
 
 user_input = st.text_input("Please enter your user id", placeholder="Please enter your user id")
 check_in_button = st.button("Check in")
 
 if check_in_button:
-    user_input
+
+    # API call to read DB and find user object aligned with that name
+    # if that doesn't exist, say no
+
+    # API call to create shift, inputting user object
+    st.write("Check in successful!")
--- pages/signout.py
+++ pages/signout.py
@@ -1,12 +1,24 @@
 import streamlit as st
 
-# read csv of users
-
-# on button click submit, check if valid user
 
 user_input = st.text_input("Enter your name")
 food_input = st.text_input("Enter lbs of food")
 submit_button = st.button("Submit")
 
 if submit_button:
-    user_input
+    st.write("Check out successful!")
+    #foodAmt = float(food_input) if food_input.isnumeric() else st.write("Please input a number")
+    # API call to read DB and find user object aligned with that name
+    # if that doesn't exist, say no
+    #currentShift = user.shiftsWorked[-1]
+    #if (currentShift.end == None ):
+        #if (foodAmt >= 0 & foodAmt <=20):
+            #currentShift.foodTaken = foodAmt
+            # API call to set currentShift.foodTaken = foodAmt
+            #currentShift.end = DateTime.now()
+            #API write updates for currentShift to DB
+            #st.write("Check out successful!")
+        #else:
+            #st.write("Enter a value between 0 and 20.")
+    #else:
+        #st.write("No shift started")
