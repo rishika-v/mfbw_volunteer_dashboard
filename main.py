4760369592286bb440e45acd92d369f2c36fbe84
rishika-v <vadlamudirishika@gmail.com>
2023-03-08 16:18:07 -0500

    format

--- main.py
+++ main.py
@@ -4,30 +4,12 @@ import pandas as pd
 
 from streamlit.components.v1 import html
 
-st.markdown(""" 
+m = st.markdown(""""
+    <style>
 div.stButton > button:first-child {
-background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
+    background-color: rgb(204, 49, 49);
 }
-.css-2trqyj:focus:not(:active) {
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}
-.css-2trqyj:focus:(:active) {
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}
-.css-2trqyj:focus:active){
-background-color: #0066cc;
-border-color: #ffffff;
-box-shadow: none;
-color: #ffffff;
-background-color: #0066cc;
-}
-""", unsafe_allow_html=True)
+</style>""", unsafe_allow_html=True)
 
 
 
